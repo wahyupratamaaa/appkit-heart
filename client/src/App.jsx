2b5cab31ca14393636f756cd6c52043da0949b30
Wahyu Pratama <email_baru@contoh.com>
2024-12-16 22:17:41 +0800

    shifuuu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1
-        className="text-1xl text-gray-600 flex items-center justify-center border border-red-800 flex-col w
-      h-screen "
-      >
-        haii puqimak audy
-      </h1>
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <h1 className="text-1xl ">Hello world!</h1>
+      </div>
     </>
   );
 }
